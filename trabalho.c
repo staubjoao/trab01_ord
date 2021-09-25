208fa6ba3a98598a74254afda0401803889d260e
unknown <staubjoao>
2021-09-25 19:51:45 -0300

    nada funciona

--- trabalho.c
+++ trabalho.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <windows.h>
+#include <stdbool.h>
 
 #define DELIM_STR '|'
 #define COMP_REG 64
@@ -14,9 +15,14 @@ struct
 
 void importacao(char argv[]);
 int le_campos(char buffer[], int size, FILE *entrada);
-int checkIf_file_exists(const char *filename);
 void leia_op(char argv[]);
+// void busca(char key[]);
+int leia_reg(char buffer[], int tam, FILE *entrada);
+int le_linha(char linha[], int len, FILE *entrada);
 void imprimePed();
+int checkIf_file_exists(const char *filename);
+void le_mostra(FILE *arq);
+bool busca_registro(char key[], int tam_reg);
 
 int main(int argc, char *argv[])
 {
@@ -99,6 +105,9 @@ void importacao(char argv[])
 
     } while (cont > 0);
 
+    rewind(saida);
+    fwrite(&cab, sizeof(cab), 1, saida);
+
     fclose(entrada);
     fclose(saida);
 }
@@ -122,29 +131,144 @@ int le_campos(char campo[], int size, FILE *entrada)
     return i;
 }
 
-int checkIf_file_exists(const char *filename)
-{
-    struct stat buffer;
-    int exist = stat(filename, &buffer);
-    if (exist == 0)
-        return 1;
-    else
-        return 0;
-}
-
 void leia_op(char argv[])
 {
     FILE *entrada;
 
     entrada = fopen(argv, "r");
 
-    int len = COMP_REG + 3;
-    char linha[len];
+    int i, j = 2, len = COMP_REG + 3, num;
+    char linha[len], num_str[6];
 
-    while(!feof(entrada))
+    //fazer o seek ler o registro fazer seek para o proximo...
+    while (!feof(entrada))
     {
         le_linha(linha, len, entrada);
-        printf("%s\n", linha);
+        switch (linha[0])
+        {
+        case 'b':
+            for (i = 0; i < 6; i++)
+            {
+                num_str[i] = linha[j];
+                j++;
+            }
+            j = 2;
+            num = atoi(num_str);
+            itoa(num, num_str, 10);
+            busca_registro(num_str, COMP_REG);
+            break;
+        case 'i':
+            break;
+        case 'r':
+            break;
+        }
+        linha[0] = '\0';
+    }
+
+    fclose(entrada);
+}
+
+bool busca_registro(char key[], int tam_reg)
+{
+    FILE *busca;
+    busca = fopen("dadaos.dat", "r+b");
+
+    bool achou = false;
+    char buffer[COMP_REG + 1], aux[COMP_REG + 1];
+    char *key_buffer;
+    ;
+    int comp_reg, var, posicao_de_leitura;//posicao_de_leitura vai ser usada depois
+
+    printf("\nBusca pelo registro de chave '%s'\n", key);
+
+    fseek(busca, sizeof(cab), SEEK_SET);
+    fread(&var, sizeof(short), 1, busca);
+    printf("%d", var);
+    while (achou == false)
+    {
+        posicao_de_leitura = ftell(busca);
+        fread(buffer, sizeof(char), tam_reg, busca);
+            printf("%s\n", buffer);
+        buffer[var] = '\0';
+
+        if (buffer[0] != '*')
+        {
+            strcpy(aux, buffer);
+            key_buffer = strtok(aux, "|");
+
+            if (strcmp(key, key_buffer))
+            {
+                achou = true;
+            }
+        }
+    }
+    if (achou)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int leia_reg(char buffer[], int tam, FILE *entrada)
+{
+    int comp_reg;
+    if (fread(&comp_reg, sizeof(int), 1, entrada) == 0)
+    {
+        return 0;
+    }
+    if (comp_reg < tam)
+    {
+        fread(buffer, sizeof(char), comp_reg, entrada);
+        buffer[comp_reg] = '\0';
+        return comp_reg;
+    }
+    else
+    {
+        printf("%d\n", comp_reg);
+        return 0;
+    }
+}
+
+// int leia_reg(char buffer[], int tam, FILE *entrada)
+// {
+//     rewind(entrada);
+
+//     int comp_reg;
+//     if (fread(&comp_reg, sizeof(int), 1, entrada) != 0)
+//     {
+//         printf("%d\n", comp_reg);
+//         return 0;
+//     }
+
+//     if (comp_reg < tam)
+//     {
+//         printf("%d\n", comp_reg);
+//         fread(buffer, sizeof(char), comp_reg, entrada);
+//         buffer[comp_reg] = '\0';
+//         return comp_reg;
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// }
+
+void le_mostra(FILE *entrada)
+{
+    char buffer[COMP_REG + 1];
+    char *campo;
+
+    fread(buffer, COMP_REG, 1, entrada);
+
+    printf("\nConteudo do registro\n");
+    campo = strtok(buffer, "|");
+    while (campo != NULL)
+    {
+        printf("%s\n", campo);
+        campo = strtok(NULL, "|");
     }
 }
 
@@ -152,7 +276,7 @@ int le_linha(char linha[], int len, FILE *entrada)
 {
     int i = 0;
     char c = fgetc(entrada);
-    if(c != EOF)
+    if (c != EOF)
     {
         while (c != '\n')
         {
@@ -162,11 +286,21 @@ int le_linha(char linha[], int len, FILE *entrada)
         }
         linha[i] = '\0';
         return i;
-    }else
+    }
+    else
         return 0;
 }
 
-
 void imprimePed()
 {
+}
+
+int checkIf_file_exists(const char *filename)
+{
+    struct stat buffer;
+    int exist = stat(filename, &buffer);
+    if (exist == 0)
+        return 1;
+    else
+        return 0;
 }
